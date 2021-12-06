3b8f016a4d1d435d3e5797d63de71477240e7212
Tordf932 <55528475+Tordf932@users.noreply.github.com>
2021-12-06 15:00:11 +0100

    added background and text

--- it-status.py
+++ it-status.py
@@ -3,13 +3,34 @@
 import os, pygame, codecs
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+pygame.init()
+pygame.font.init()
+
 host_name = '10.0.2.15'  # IP Address of Raspberry Pi
 host_port = 8000
-
-pygame.init()
-screen = pygame.display.set_mode((1920, 1080))
-font = pygame.font.SysFont("Arial", 20)
+sw = 1920
+sh = 1080
+screen = pygame.display.set_mode((sw, sh))
 clock = pygame.time.Clock()
+bg = pygame.image.load("assets/bg.png")
+font1 = pygame.font.SysFont("None", 200)
+font2 = pygame.font.SysFont("None", 100)
+font3 = pygame.font.SysFont("None", 50)
+
+text_ledig = font1.render("LEDIG", False, (255,255,255))
+text_rect1 = text_ledig.get_rect(center=(sw/2, sh/2))
+text_ledig2 = font1.render("KOM INN!", False, (255,255,255))
+text_rect12 = text_ledig2.get_rect(center=(sw/2, sh/1.6))
+
+text_mote = font1.render("SITTER I MØTE", False, (255,255,255))
+text_rect2 = text_mote.get_rect(center=(sw/2, sh/2))
+text_mote2 = font2.render("KOM TILBAKE SENERE", False, (255,255,255))
+text_rect22 = text_mote2.get_rect(center=(sw/2, sh/1.6))
+
+text_annet = font1.render("VI HAR ANNET OPPDRAG", False, (255,255,255))
+text_rect3 = text_annet.get_rect(center=(sw/2, sh/2))
+text_annet2 = font3.render("RING: 35919209", False, (255,255,255))
+text_rect32 = text_annet2.get_rect(center=(sw/2, sh/1.6))
 
 white = [255, 255, 255]
 red = [255, 0, 0]
@@ -44,9 +65,9 @@ class MyServer(BaseHTTPRequestHandler):
            <h1>IT-KONTOR STATUS</h1>
            <form action="/" method="POST">
                Status:
-               <input type="submit" name="submit" value="LEDIG">
-               <input type="submit" name="submit" value="MØTE">
-               <input type="submit" name="submit" value="ANNET OPPDRAG">
+		<button type="submit" name="submit" value="ledig">LEDIG</button>
+		<button type="submit" name="submit" value="mote">MØTE</button>
+		<button type="submit" name="submit" value="annet">ANNET OPPDRAG</button>
            </form>
            </body>
            </html>
@@ -60,11 +81,11 @@ class MyServer(BaseHTTPRequestHandler):
         post_data = self.rfile.read(content_length).decode("iso-8859-1")
         post_data = post_data.split("=")[1]
 
-        if post_data == 'LEDIG':
+        if post_data == 'ledig':
             ledig()
-        if post_data == 'MØTE':
+        if post_data == 'mote':
             mote()
-        if post_data == 'ANNET OPPDRAG':
+        if post_data == 'annet':
             annet_oppdrag()
         else:
             pygame.display.update()
@@ -73,15 +94,21 @@ class MyServer(BaseHTTPRequestHandler):
         self._redirect('/')  # Redirect back to the root url
 
 def ledig():
-    screen.fill(red)
+    screen.blit(bg, (0,0))
+    screen.blit(text_ledig, text_rect1)
+    screen.blit(text_ledig2, text_rect12)
     pygame.display.update()
 
 def mote():
-    screen.fill(red)
+    screen.blit(bg, (0,0))
+    screen.blit(text_mote, text_rect2)
+    screen.blit(text_mote2, text_rect22)
     pygame.display.update()
 
 def annet_oppdrag():
-    screen.fill(red)
+    screen.blit(bg, (0,0))
+    screen.blit(text_annet, text_rect3)
+    screen.blit(text_annet2, text_rect32)
     pygame.display.update()
 
 # # # # # Main # # # # #
